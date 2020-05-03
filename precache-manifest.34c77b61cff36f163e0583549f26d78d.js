e692ee8bba1b9d0591e2911c808e6bbbf86d540c
biszczak <dbiszczak@gmail.com>
2020-05-03 16:04:40 +0200

    Updates

---
+++ precache-manifest.34c77b61cff36f163e0583549f26d78d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f261adb1052d0f4cf3537c2d92096051",
+    "url": "/companies-grid/index.html"
+  },
+  {
+    "revision": "0570faa838e334343fab",
+    "url": "/companies-grid/static/css/main.6b19aeac.chunk.css"
+  },
+  {
+    "revision": "b4f02e3107cea99adefc",
+    "url": "/companies-grid/static/js/2.60d2a8d0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/companies-grid/static/js/2.60d2a8d0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0570faa838e334343fab",
+    "url": "/companies-grid/static/js/main.477fe3a1.chunk.js"
+  },
+  {
+    "revision": "66268888d46b8edf14b8",
+    "url": "/companies-grid/static/js/runtime-main.75758b50.js"
+  }
+]);
